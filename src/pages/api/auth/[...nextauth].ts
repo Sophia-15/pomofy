88874bc68353e6d779b363ddd765ec2a0ffab241
sophia-15 <deletrasopa@gmail.com>
2022-02-07 00:22:26 -0300

    :bug: trying to fix login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,29 +1,6 @@
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
-import { LOGIN_URL, spotifyAPI } from '../../../services/spotify';
-
-async function refreshAccessToken(token: any) {
-  try {
-    spotifyAPI.setAccessToken(token.accessToken);
-    spotifyAPI.setRefreshToken(token.refreshToken);
-
-    const { body: refreshedToken } = await spotifyAPI.refreshAccessToken();
-
-    return {
-      ...token,
-      accessToken: refreshedToken.access_token,
-      accessTokenExpires: Date.now() + refreshedToken.expires_in * 3600,
-      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
-    };
-  } catch (error) {
-    console.log(error);
-
-    return {
-      ...token,
-      error: 'RefreshAccessTokenError',
-    };
-  }
-}
+import { LOGIN_URL } from '../../../services/spotify';
 
 export default NextAuth({
   providers: [
@@ -36,36 +13,5 @@ export default NextAuth({
   secret: process.env.JWT_SECRET,
   pages: {
     signIn: '/login',
-  },
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user && account.expires_at) {
-        return {
-          ...token,
-          accessToken: account.access_token,
-          accessTokenExpires: Date.now() + account.expires_at * 1000,
-          refreshToken: account.refresh_token,
-          user,
-        };
-      }
-
-      if (Date.now() < token.accessTokenExpires) {
-        return token;
-      }
-
-      return refreshAccessToken(token);
-    },
-    async session({ session, token }) {
-      session.user = {
-        name: token.name,
-        email: token.email,
-        image: token.picture,
-      };
-      session.refreshToken = token.refreshToken;
-      session.accessToken = token.accessToken;
-      session.error = token.error;
-
-      return session;
-    },
   },
 });
